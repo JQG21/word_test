99da152ce5430d6b49ff027a9d7fa5c76263045a
succeessful <18212325202@163.com>
2020-10-26 20:26:17 +0800

    10.16test

--- app/src/main/java/com/example/word_test/MainActivity.java
+++ app/src/main/java/com/example/word_test/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.word_test;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.word_test.word.WordContent;
+
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnItemClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemClick(WordContent.WordItem wordItem) {
+        DetailFragment detailFragment = (DetailFragment)getSupportFragmentManager().findFragmentById(R.id.wordDetail);
+
+        if(detailFragment !=null){
+            detailFragment.updataView(wordItem);
+        }
+        else {
+            DetailFragment newFragment = new DetailFragment();
+            Bundle args = new Bundle();
+
+            args.putSerializable(DetailFragment.ARG_PARAM,wordItem);
+            newFragment.setArguments(args);
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.wordDetail,newFragment);
+            transaction.commit();
+        }
     }
 }
--- app/src/main/java/com/example/word_test/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/com/example/word_test/MyItemRecyclerViewAdapter.java
@@ -31,7 +31,7 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder,final int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         holder.mWordView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
